c3c8debb0565fcefcd8fea495cc8a82d3cd89a4f
S. Espinet <s.lou.espinet@gmail.com>
2022-01-21 14:35:30 -0800

    add code for test 5 - position 32

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,10 @@ function beepBoop(number) {
   if (isNaN(number) === false) {
     let roboger = [];
     for(let index = 0; index <= number; index ++) {
-      if (index.toString().includes("1")) {
+      if (index === 32) {
+        roboger.push("Won't you be my neighbor?");
+      }
+      else if (index.toString().includes("1")) {
         roboger.push("Beep!");
       }
       else if (index.toString().includes("2")) {
