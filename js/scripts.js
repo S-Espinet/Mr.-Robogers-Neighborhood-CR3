ce7bd86f4c0ae82f4bbeff32b8b0c248f27037e9
S. Espinet <s.lou.espinet@gmail.com>
2022-01-21 14:03:38 -0800

    WIP: trying to write code for test 4 - 1,2,3 outputs

--- js/scripts.js
+++ js/scripts.js
@@ -3,10 +3,16 @@ function beepBoop(number) {
   if (isNaN(number) === false) {
     let roboger = [];
     for(let index = 0; index <= number; index ++) {
-      roboger.push(index);
+      roboger.push(index.toString());
     }
-    return roboger;
+    let robogerString = roboger.toString();
+    return robogerString;
+    if (robogerString.includes("1") {
+        
+    })
   }else {
     return "Please enter a valid number.";
   }
 }
+
+//Javascript method to search string - includes
