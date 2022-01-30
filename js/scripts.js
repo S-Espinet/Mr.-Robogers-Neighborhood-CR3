777d09bbbd5a7e4aed9636637225f8ccf44c0db7
S. Espinet <s.lou.espinet@gmail.com>
2022-01-29 16:03:10 -0800

    change js to reflect prompt intentions

--- js/scripts.js
+++ js/scripts.js
@@ -3,23 +3,14 @@ function beepBoop(number) {
   if (isNaN(number) === false) {
     let roboger = [];
     for(let index = 0; index <= number; index ++) {
-      if (index === 32) {
+      if (index.toString().includes("3")) {
         roboger.push("Won't you be my neighbor?");
-      }
-      else if (number < 32 && index === 21) {
-        roboger.push("Boop!");
-      }
-      else if (number < 21 && index === 13) {
-        roboger.push("Won't you be my neighbor?");
-      }
-      else if (index.toString().includes("1")) {
-        roboger.push("Beep!");
       }
       else if (index.toString().includes("2")) {
         roboger.push("Boop!");
       }
-      else if (index.toString().includes("3")) {
-        roboger.push("Won't you be my neighbor?")
+      else if (index.toString().includes("1")) {
+        roboger.push("Beep")
       }
       else {
         roboger.push(index.toString());
